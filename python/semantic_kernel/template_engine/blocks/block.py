8886b4a77e47752ef74fcfc4b83800fb45004261
Aditya Gudimella <aditya.gudimella@gmail.com>
2023-06-26 16:48:14 -0700

    Upgrade test_block

--- python/semantic_kernel/template_engine/blocks/block.py
+++ python/semantic_kernel/template_engine/blocks/block.py
@@ -1,18 +1,24 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from typing import Tuple
+import typing as t
 
 import pydantic as pdt
 
 from semantic_kernel.logging_ import NullLogger, SKLogger
+from semantic_kernel.orchestration.context_variables import ContextVariables
 from semantic_kernel.pydantic_ import SKBaseModel
 from semantic_kernel.template_engine.blocks.block_types import BlockTypes
 
 
 class Block(SKBaseModel):
-    content: pdt.constr(strip_whitespace=True) = ""
+    content: pdt.constr(
+        strip_whitespace=True
+    ) = ""  # pyright: ignore[reportGeneralTypeIssues]
     logger: SKLogger = pdt.Field(default_factory=NullLogger)
     type: BlockTypes = BlockTypes.UNDEFINED
 
-    def is_valid(self) -> Tuple[bool, str]:
+    def is_valid(self) -> t.Tuple[bool, str]:
+        return True, ""
+
+    def render(self, variables: t.Optional[ContextVariables]) -> str:
         raise NotImplementedError("Subclasses must implement this method.")
--- python/tests/unit/template_engine/blocks/test_block.py
+++ python/tests/unit/template_engine/blocks/test_block.py
@@ -1,26 +1,21 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-from logging import Logger
+import pytest
 
-from pytest import raises
-
-from semantic_kernel.logging_ import NullLogger
 from semantic_kernel.template_engine.blocks.block import Block
 from semantic_kernel.template_engine.blocks.block_types import BlockTypes
 
 
-def test_init():
-    block = Block(content="test content", logger=NullLogger())
+def test_init_content_whitespace():
+    """When I initialize a block with whitespace content, it should be stripped"""
+    block = Block(content="  test content  ")
     assert block.content == "test content"
-    assert isinstance(block.logger, Logger)
 
 
-def test_type_property():
-    block = Block()
+@pytest.fixture()
+def block() -> Block:
+    return Block()
+
+
+def test_type_property(block: Block):
     assert block.type == BlockTypes.UNDEFINED
-
-
-def test_is_valid_not_implemented():
-    block = Block()
-    with raises(NotImplementedError):
-        block.is_valid()
